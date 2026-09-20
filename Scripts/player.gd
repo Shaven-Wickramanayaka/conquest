7cb0609864dc50fbd287d8e676d593001fdae638
Shaven Wickramanayaka <shavendulneth@gmail.com>
2026-09-20 20:15:43 +0100

    made level

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 1000.0
+const JUMP_VELOCITY = -900.0
+const gravMultiplier = 3;
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * gravMultiplier
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
